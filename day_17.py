9f3bb80ae3972a0acb13a7809ff78b4483302185
Mike Hepburn <eformat@gmail.com>
2025-08-05 16:55:55 +1000

    🏌️ Few-Shot Optimization🏌️

--- day_17.py
+++ day_17.py
@@ -1,13 +1,45 @@
 import os
 
 import dspy
+import mlflow
+from rich.pretty import pprint
+
+mlflow.set_tracking_uri("http://localhost:5500")
+mlflow.set_experiment("20_days_dspy")
+mlflow.dspy.autolog(
+    log_compiles=True,    # Track optimization process
+    log_evals=True,       # Track evaluation results
+    log_traces_from_compile=True  # Track program traces during optimization
+)
+
+# 1. Configure the Language Model
+#    Replace 'YOUR_OPENAI_API_KEY' with your actual OpenAI API key.
+#    You can also set it as an environment variable: export OPENAI_API_KEY='your_key_here'
+#    If using a local LM with Ollama or SGLang, adjust api_base and api_key accordingly.
+
+LLM_URL=os.getenv('LLM_URL', 'http://localhost:8080/v1')
+API_KEY=os.getenv('API_KEY', 'fake')
+LLM_MODEL=os.getenv('LLM_MODEL', 'openai/models/Llama-3.2-3B-Instruct-Q8_0.gguf')
+MAX_TOKENS=os.getenv('MAX_TOKENS', 3000)
+TEMPERATURE=os.getenv('TEMPERATURE', 0.2)
+dspy.enable_logging()
+
+lm = dspy.LM(model=LLM_MODEL,
+             api_base=LLM_URL,
+             api_key=API_KEY,
+             temperature=TEMPERATURE,
+             model_type='chat',
+             stream=False)
+dspy.configure(lm=lm)
+dspy.settings.configure(track_usage=True)
 
 # Ensure LM and RM are configured
-lm = dspy.LM(
-    "openai/gpt-4o-mini",
-    api_key=os.environ.get("OPENAI_API_KEY", "YOUR_OPENAI_API_KEY"),
-)
-dspy.configure(lm=lm)
+# lm = dspy.LM(
+#     "openai/gpt-4o-mini",
+#     api_key=os.environ.get("OPENAI_API_KEY", "YOUR_OPENAI_API_KEY"),
+# )
+# dspy.configure(lm=lm)
+
 colbertv2_wiki17_abstracts = dspy.ColBERTv2(
     url="http://20.102.90.50:2017/wiki17_abstracts"
 )
@@ -68,11 +100,11 @@ devset_rag = [
 print("\n--- Step 1: Define a Metric for RAG ---")
 
 
-# 4. Define a RAG-specific metric (answer exact match from documentation)
+# 4. Define a RAG-specific metric (answer close match !)
 def answer_exact_match(example, pred, trace=None):
     """Evaluates if the predicted answer exactly matches the gold answer (case-insensitive)."""
-    return example.answer.lower() == pred.answer.lower()
-
+    pprint("metric>>> " + example.answer.lower() + ":" + pred.answer.lower())
+    return example.answer.lower() in pred.answer.lower()
 
 print(f"Metric function defined: {answer_exact_match.__name__}")
 
